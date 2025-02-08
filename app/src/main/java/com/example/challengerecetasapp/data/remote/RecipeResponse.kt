3f8fc7886eb80094a7e6fd16b6d5799fd7812a87
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2025-02-08 14:55:14 -0500

    added parameters origin

--- app/src/main/java/com/example/challengerecetasapp/data/remote/RecipeResponse.kt
+++ app/src/main/java/com/example/challengerecetasapp/data/remote/RecipeResponse.kt
@@ -16,5 +16,8 @@ data class RecipeResponse (
     @SerializedName("difficulty") val difficulty: Int = 0,
     @SerializedName("image") val imageUrl: String = "",
     @SerializedName("is_favorite") val isFavorite: Boolean = false,
-    @SerializedName("category_code") val categoryCode: String = ""
+    @SerializedName("category_code") val categoryCode: String = "",
+    @SerializedName("latitude") val latitude: Double = 0.0,
+    @SerializedName("longitude") val longitude: Double = 0.0,
+    @SerializedName("origin") val createdAt: String = "",
 )
